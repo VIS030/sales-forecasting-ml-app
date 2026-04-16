715c6b01bd8aae8efa41448e1138b8ddad11368d
VISHAV RANA <vishavrana333@gmail.com>
2026-04-16 09:49:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -650,6 +650,6 @@ def api_predict():
     }
     return jsonify(response)
 
-
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
